462603be3cf38946e6d39dc2b3f08e294d51236a
Taylor McKinnon <mail@tmacs.space>
2018-05-29 12:05:44 -0700

    move back to sh cript to start orbit-sim

---
+++ eve/workers/start_orbit_simulator.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "helm upgrade ciutil
+          --install orbit-simulator/orbit-simulator
+          --wait $(./ci_env.sh orbit-simulator)"
+
+helm upgrade ciutil --install orbit-simulator/orbit-simulator --wait $(./ci_env.sh orbit-simulator)"
