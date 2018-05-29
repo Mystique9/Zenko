99049b6886922f3a5456c2fb601bf9a66fd8a005
Taylor McKinnon <mail@tmacs.space>
2018-05-29 13:35:21 -0700

    fix unquoted string

--- eve/workers/start_orbit_simulator.sh
+++ eve/workers/start_orbit_simulator.sh
@@ -5,4 +5,4 @@ echo "helm upgrade ciutil
           --install orbit-simulator/orbit-simulator
           --wait $(./ci_env.sh orbit-simulator)"
 
-helm upgrade ciutil --install orbit-simulator/orbit-simulator --wait $(./ci_env.sh orbit-simulator)"
+helm upgrade ciutil --install orbit-simulator/orbit-simulator --wait $(./ci_env.sh orbit-simulator)
