3450e55783843813c90519fb4e30bfd62f58549c
Taylor McKinnon <mail@tmacs.space>
2018-05-31 10:59:02 -0700

    try to dump all logs again

--- eve/workers/start_orbit_simulator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "$(env)"
-echo "helm upgrade ciutil
-          --install orbit-simulator/orbit-simulator
-          --wait $(./ci_env.sh orbit-simulator)"
-
-helm upgrade ciutil --install orbit-simulator/orbit-simulator --wait $(./ci_env.sh orbit-simulator)
